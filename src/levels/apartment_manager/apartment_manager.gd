667e346d0464e0403526d1d82331f1060487a5e9
NNE9376 <vensovn@gmail.com>
2026-07-18 09:08:40 +0300

    chore(apartment_manager): Добавь обновленную логику работы областей дверей
    -- Добавлена проверка на Игрока

--- src/levels/apartment_manager/apartment_manager.gd
+++ src/levels/apartment_manager/apartment_manager.gd
@@ -6,6 +6,8 @@ var current_appartment: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#$Floor1/Apartment1/DialogueBox/MarginContainer/Label.text = Dwellers.common_dialog_lines[6]
+	
 	$Floor1/Apartment1.body_entered.connect(_on_entered_apartment.bind(1))
 	$Floor1/Apartment2.body_entered.connect(_on_entered_apartment.bind(2))
 	$Floor2/Apartment3.body_entered.connect(_on_entered_apartment.bind(3))
@@ -26,10 +28,12 @@ func _process(_delta: float) -> void:
 	pass
 
 
-func _on_entered_apartment(_body, apartment_number) -> void:
+func _on_entered_apartment(body: Node2D, apartment_number: int) -> void:
+	print("apartment_manager._on_entered_apartment.apartment_number: ", apartment_number)
 	current_appartment = apartment_number
-	Events.player_entered_apartment.emit(current_appartment)
+	if body.name == "Player":
+		Events.player_entered_apartment.emit(current_appartment)
 
 
-func _on_exited_apartment() -> void:
+func _on_exited_apartment(_body: Node2D) -> void:
 	Events.player_exited_apartment.emit()
