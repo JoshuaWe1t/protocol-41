c85fe5023bbc3461f02b8a979c25d3eab794752f
NNE9376 <vensovn@gmail.com>
2026-07-21 09:12:20 +0300

    feat: Удали управление персонажем
    -- Все управление персонажем перенесено в узлы состояний

--- src/gameplay/player/player.gd
+++ src/gameplay/player/player.gd
@@ -1,19 +1,14 @@
 extends CharacterBody2D
 
-
-# Загружаем сцену диалогового окна один раз при старте игры
 const DIALOGUE_BUBBLE = preload("res://src/core/dialogue_box/dialogue_box.tscn")
-
+const SPEED: float = 300.0
 
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
-@onready var sprite: Sprite2D = $Sprite2D # Ссылка на спрайт для разворота
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var dialogue_box: PanelContainer = $DialogueBox
 @onready var dialogue_label: Label = $DialogueBox/DialogueBox
 @onready var dialogue_timer: Timer = $DialogueBox/DialogueTimer
-
-
-const SPEED: float = 300.0
-
+@onready var state_machine: StateMachine = $StateMachine # Ссылка на твою машину состояний
 
 var current_floor: int
 var at_stairs: bool = false
@@ -22,106 +17,61 @@ var can_down: bool
 var at_lift: bool = false
 var can_use_lift: bool 
 var is_lift_broken: bool = false
-# Переменная, которая управляет возможностью ходить
-var is_frozen: bool = false
-# Запоминаем нормальный масштаб игрока (обычно это 1, 1)
-var normal_scale: Vector2 = Vector2(1.0, 1.0)
 var current_apartment: int
 var at_door: bool = false
-var is_knocking: bool = false
-
 
 func _ready() -> void:
-	print("PlayerPosition: ", self.position)
-	anim_player.play("idle")
-	
-	# Прячем диалог при старте на всякий случай
 	dialogue_box.hide() 
-	
-	# Подключаем таймер
 	dialogue_timer.timeout.connect(_on_dialogue_timeout)
-	# Подключаемся к сигналу
+	
+	# Подписки на сигналы окружения
 	Events.player_entered_floor.connect(_on_entered_floor)
 	Events.player_entered_stairs.connect(_on_entered_stairs)
 	Events.player_exited_stairs.connect(_on_exited_stairs)
 	Events.player_entered_lift.connect(_on_entered_lift)
 	Events.player_exited_lift.connect(_on_exited_lift)
 	Events.lift_crashed.connect(_on_lift_denied)
-	# Подписываемся на новые сигналы камеры
 	Events.camera_transition_started.connect(_on_camera_started)
 	Events.camera_transition_finished.connect(_on_camera_finished)
-	# Подписываемся под новые сигналы для взаимодействия с дверьми
 	Events.player_entered_apartment.connect(_on_entered_apartment)
 	Events.player_exited_apartment.connect(_on_exited_apartment)
 	Events.get_text_line.connect(_on_get_text_line)
+	Events.player_entered_spore_area.connect(_on_entered_spore)
+	Events.player_exited_spore_area.connect(_on_exited_spore)
 
 
-func _physics_process(_delta: float) -> void:
-
-	var direction := Input.get_axis("backward", "forward")
-	# ЕСЛИ ИГРОК ЗАМОРОЖЕН, МЫ ПРЕРЫВАЕМ ФУНКЦИЮ И НЕ ПРИНИМАЕМ НАЖАТИЯ КЛАВИШ
-	if is_frozen:
-		velocity = Vector2.ZERO 
-		anim_player.play("idle")
-		move_and_slide()
-		return 
-	elif is_frozen and at_door:
-		velocity = Vector2.ZERO
-		
-	
-	if direction:
-		velocity.x = direction * SPEED
-		if not is_knocking: # <--- Добавили проверку
-			anim_player.play("walk")
-		sprite.flip_h = (direction < 0)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if not is_knocking: # <--- Добавили проверку
-			anim_player.play("idle")
-	
-	# Обработка движения ВВЕРХ
-	if Input.is_action_just_pressed("go_up"):
-		if can_up:
-			self.position.y -= 720
-			print("PlayerPosition: ", self.position)
-			dialogue_box.hide() 
-		elif at_stairs: # <--- ДОБАВЛЕНО: Показываем диалог, только если стоим у лестницы
-			show_dialogue("Выше подниматься некуда...")
-
-	# Обработка движения ВНИЗ
-	if Input.is_action_just_pressed("go_down"):
-		if can_down:
-			self.position.y += 720
-			print("PlayerPosition: ", self.position)
-			dialogue_box.hide()
-		elif at_stairs: # <--- ДОБАВЛЕНО: Показываем диалог, только если стоим у лестницы
-			show_dialogue("Спускаться некуда...")
+func try_go_up() -> void:
+	if can_up:
+		position.y -= 720
+		dialogue_box.hide() 
+	elif at_stairs:
+		show_dialogue("Выше подниматься некуда...")
 
 
-	if Input.is_action_just_pressed("interact"):
-		if can_use_lift:
-			self.position.y += 1440
-			print("PlayerPosition: ", self.position)
-			dialogue_box.hide()
-		elif is_lift_broken and at_lift:
-			show_dialogue("Лифт застрял и больше не работает")
-		elif at_lift:
-			show_dialogue("Лифт сейчас на третьем этаже")
-	
-	
-	if Input.is_action_just_pressed("interact"):
-		print("Interact")
-		if at_door and not is_knocking: # <--- Проверяем, что уже не стучим
-			is_knocking = true          # Включаем блокировку анимаций
-			anim_player.play("knock")   # Запускаем стук
-			
-			# Ждем завершения именно этой анимации
-			await anim_player.animation_finished 
-			
-			is_knocking = false         # Снимаем блокировку
-			Events.get_text_line.emit(current_floor, current_apartment)
+func try_go_down() -> void:
+	if can_down:
+		position.y += 720
+		dialogue_box.hide()
+	elif at_stairs:
+		show_dialogue("Спускаться некуда...")
 
-	move_and_slide()
+
+func try_use_lift() -> void:
+	if can_use_lift:
+		position.y += 1440
+		dialogue_box.hide()
+	elif is_lift_broken and at_lift:
+		show_dialogue("Лифт застрял и больше не работает")
+	elif at_lift:
+		show_dialogue("Лифт сейчас на третьем этаже")
+
+
+func _on_camera_started() -> void:
+	state_machine.change_state(state_machine.get_node("FrozenState"))
+
+
+func _on_camera_finished() -> void:
+	state_machine.change_state(state_machine.get_node("IdleState"))
 
 
 func _on_entered_stairs(is_can_up: bool, is_can_down: bool) -> void:
@@ -129,6 +79,7 @@ func _on_entered_stairs(is_can_up: bool, is_can_down: bool) -> void:
 	can_up = is_can_up
 	can_down = is_can_down
 	at_stairs = true # <--- ДОБАВЛЕНО: Игрок подошел к лестнице
+
 
 func _on_exited_stairs() -> void:
 	print("Игрок вышел из зоны лестницы")
@@ -154,17 +105,6 @@ func _on_entered_floor(floor_number: int) -> void:
 	print("Игрок на этаже: ", current_floor)
 
 
-# Функции-обработчики сигналов камеры
-func _on_camera_started() -> void:
-	is_frozen = true
-	# Здесь также можно остановить анимацию бега игрока, 
-	# чтобы он не "бежал на месте" во время движения камеры.
-	# $AnimatedSprite2D.play("idle") 
-
-func _on_camera_finished() -> void:
-	is_frozen = false
-
-
 func show_dialogue(text: String) -> void:
 	dialogue_label.text = text
 	dialogue_box.show()
@@ -186,9 +126,18 @@ func _on_entered_apartment(apartment_number: int) -> void:
 	at_door = true
 	print("Вход в зону кв ", apartment_number, " | at_door: ", at_door)
 
+
 func _on_exited_apartment() -> void:
 	at_door = false
 	print("Выход из зоны кв | at_door: ", at_door)
+
+
+func _on_entered_spore() -> void:
+	pass
+
+
+func _on_exited_spore() -> void:
+	pass
 
 
 func _on_get_text_line(floor_number: int, apartment_number: int) -> void:
@@ -212,17 +161,11 @@ func _on_get_text_line(floor_number: int, apartment_number: int) -> void:
 	spawn_door_dialogue(Vector2(dialog_box_pox_x,dialog_box_pox_y), dialogue_line)
 
 
-# Эта функция вызывается, когда игрок успешно постучал в дверь
 func spawn_door_dialogue(door_global_position: Vector2, text: String = "...") -> void:
-	# 2. Создаем копию загруженной сцены
 	var bubble_instance = DIALOGUE_BUBBLE.instantiate()
 	
-	# 3. Добавляем облачко на основную сцену игры.
-	# Лучше всего добавлять его в текущую сцену (current_scene), а не внутрь двери.
-	# Если добавить внутрь двери, текст может случайно отмасштабироваться вместе с ней.
 	get_tree().current_scene.add_child(bubble_instance)
 	
-	# 4. Передаем координаты (например, позицию двери, поднятую чуть выше) и текст
 	var offset_position = door_global_position + Vector2(0, -20) # Поднимаем на 20 пикселей выше центра двери
 	
 	bubble_instance.setup(text, offset_position)
@@ -239,8 +182,6 @@ func get_unique_dialogue_line(floor_number: int, apt_number: int) -> String:
 	for apt in apartments:
 		if apt["apartment_number"] == apt_number:
 			
-			# === НОВАЯ ЛОГИКА: Таймер кулдауна на 15 секунд ===
-			
 			# Получаем текущее время работы игры в миллисекундах
 			var current_time = Time.get_ticks_msec()
 			
@@ -253,8 +194,6 @@ func get_unique_dialogue_line(floor_number: int, apt_number: int) -> String:
 				
 			# Обновляем таймер: записываем текущее время как момент последнего стука
 			apt["last_interaction_time"] = current_time
-			
-			# =================================================
 			
 			# Получаем доступ к массиву фраз
 			var dialog_lines: Array = apt["dweller"]["full_dialog_lines"]
