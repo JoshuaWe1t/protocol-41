2118d3d6aab89eb6852ffc258a8e7a1311eaf309
NNE9376 <vensovn@gmail.com>
2026-07-21 09:03:05 +0300

    feat(state_machine): Добавь сущность игрока в машину состояниий

--- src/core/state_machines/state.gd
+++ src/core/state_machines/state.gd
@@ -1,16 +1,22 @@
 class_name State extends Node
 
+
+var player: CharacterBody2D 
+
 @warning_ignore("unused_signal")
 signal switch_state(state: State)
 
 func enter_state() -> void:
-	pass
-	
+    pass
+    
+
 func exit_state() -> void:
-	pass
-	
+    pass
+    
+
 func update(_delta: float) -> void:
-	pass
+    pass
+    
 	
 func physics_process(_delta: float) -> void:
-	pass
+    pass
--- src/core/state_machines/state_machine.gd
+++ src/core/state_machines/state_machine.gd
@@ -2,16 +2,22 @@ class_name StateMachine extends Node
 
 @export var initial_state: State
 
-var active_state: State:
-	set(new_value):
-		active_state = new_value
-		#print("Change to ", active_state)
+var active_state: State
 
 func _ready() -> void:
+	# Ждем, пока родительский узел (Player) полностью загрузится 
+	# и инициализирует все свои @onready переменные
+	await get_parent().ready
+	# Получаем ссылку на узел, к которому прикреплена машина состояний (Player)
+	var player = get_parent()
+	
 	for child_state: State in get_children():
+		# Передаем ссылку на игрока в каждое состояние
+		child_state.player = player
 		child_state.switch_state.connect(change_state)
 	
 	change_state(initial_state)
+
 
 func _process(delta: float) -> void:
 	if active_state:
@@ -22,13 +28,14 @@ func _physics_process(delta: float) -> void:
 	if active_state:
 		active_state.physics_process(delta)
 
+
 func change_state(new_state: State) -> void:
 	if new_state == active_state:
 		return
 		
 	if active_state:
 		active_state.exit_state()
-		
+	
 	active_state = new_state
 	
 	if active_state:
