4b6a2fda9c2bff2508368d9725fe2fcb509c245d
NNE9376 <vensovn@gmail.com>
2026-07-21 09:03:48 +0300

    chore(events): Добавь комментирование неиспользуемых сигналов

--- src/core/autoload/events.gd
+++ src/core/autoload/events.gd
@@ -39,15 +39,16 @@ signal player_exited_spore_area()
 #signal player_died
 
 # Сигналы для UI
-@warning_ignore("unused_signal")
-signal floor_changed(floor_number)
-@warning_ignore("unused_signal")
-signal item_collected(item_name)
+#@warning_ignore("unused_signal")
+#signal floor_changed(floor_number)
+#@warning_ignore("unused_signal")
+#signal item_collected(item_name)
 
 @warning_ignore("unused_signal")
 signal camera_transition_started
 @warning_ignore("unused_signal")
 signal camera_transition_finished
+
 
 func _ready():
 	print("Events система инициализирована")
