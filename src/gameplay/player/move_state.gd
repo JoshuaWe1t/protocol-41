656f2c7c7238156935456f9a9e9e934cc375669e
NNE9376 <vensovn@gmail.com>
2026-07-21 09:11:50 +0300

    feat(control): Добавь управление персонажем для состояния

--- src/gameplay/player/move_state.gd
+++ src/gameplay/player/move_state.gd
@@ -1,16 +1,30 @@
 extends State
 
-
-@export var move_state: State
-
+@export var idle_state: State
+@export var knock_state: State
 
 func enter_state() -> void:
-	pass # play animation
+	player.anim_player.play("walk")
 
-
-func update(_delta: float) -> void:
-	if Input.is_action_just_pressed("backward"):
-		switch_state.emit(move_state)
+func physics_process(_delta: float) -> void:
+	var direction := Input.get_axis("backward", "forward")
+	
+	if direction == 0:
+		switch_state.emit(idle_state)
+		return
 		
-	if Input.is_action_just_pressed("forward"):
-		switch_state.emit(move_state)
+	player.velocity.x = direction * player.SPEED
+	player.sprite.flip_h = (direction < 0)
+	
+	# Можно ли взаимодействовать на ходу? Если да:
+	if Input.is_action_just_pressed("go_up"):
+		player.try_go_up()
+	elif Input.is_action_just_pressed("go_down"):
+		player.try_go_down()
+	elif Input.is_action_just_pressed("interact"):
+		if player.at_door:
+			switch_state.emit(knock_state)
+		elif player.at_lift:
+			player.try_use_lift()
+			
+	player.move_and_slide()
