a5a4f2d9e47c53856308237551f6d4da59c5ddb3
NNE9376 <vensovn@gmail.com>
2026-07-22 21:03:37 +0300

    feat(ui): Добавить HUD UI сцены
    -- Добавлен скрипт для обработки UI
    -- Добавлены основные компоненты UI

---
+++ src/ui/hud.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer # Это скрипт твоего главного HUD
+
+# 1. Загружаем текстуру предмета. 
+# Укажи здесь точный путь до твоей картинки зелья, аптечки или шестеренки!
+const ITEM_GEAR_TEXTURE = preload("res://icon.svg")
+const ITEM_MEDKIT_TEXTURE = preload("res://icon.svg")
+
+# 2. Получаем ссылки на наши слоты (пути могут немного отличаться, проверь свои)
+@onready var slot_1 = $UIRoot/HBoxContainer/ItemSlot
+@onready var slot_2 = $UIRoot/HBoxContainer/ItemSlot2
+
+func _ready() -> void:
+	# 3. Вызываем функцию setup у первого слота
+	# Передаем картинку шестеренки и говорим, что у нее 3 заряда
+	slot_1.setup(ITEM_GEAR_TEXTURE, 3)
+	
+	# Можно сразу настроить и второй слот, дав ему другой предмет и 5 зарядов
+	slot_2.setup(ITEM_MEDKIT_TEXTURE, 5)
