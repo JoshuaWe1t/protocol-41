705591bf32a2f9cc51307d5d1e1d5eea2c038f7f
NNE9376 <vensovn@gmail.com>
2026-07-25 20:20:50 +0300

    feat(use_items): Добавить развилку для предметов

--- src/gameplay/player/use_item_state.gd
+++ src/gameplay/player/use_item_state.gd
@@ -13,7 +13,7 @@ func enter_state() -> void:
 	var charges_cnt = Global.item_charges.get(current_used_item_id, 0)
 	var has_charges = charges_cnt > 0
 	var item_name = Global.item_names.get(current_used_item_id, 0)
-	
+
 	if not has_charges:
 		player.show_dialogue("У %s не осталось зарядов" % item_name)
 	
@@ -21,20 +21,55 @@ func enter_state() -> void:
 		var sly_charges = "зарядов" if charges_cnt > 1 else "заряд"
 		player.show_dialogue("%s Сейчас на перезарядке. Осталось %d %s" % [item_name, charges_cnt, sly_charges])
 
-		
 	if is_on_cooldown or not has_charges:
 		print("Предмет на перезарядке или нет зарядов!")
 		# Отменяем использование и сразу возвращаемся в idle
 		# call_deferred нужен, чтобы безопасно сменить состояние в том же кадре
 		call_deferred("emit_signal", "switch_state", idle_state)
 		return
-		
+	
 	# Если проверки пройдены, проигрываем всё как обычно
 	player.velocity = Vector2.ZERO
 	player.anim_player.play("use_item")
 	
+	match current_used_item_id:
+		1:
+			get_spore_level(current_used_item_id)
+		2:
+			pass
+		3:
+			pass
+		_:
+			print("Неполучен ID предмета")
+	
+	#if player.at_spore_area:
+		#print("get_current_spore_level")
+		#if Global.current_spore_level:
+			#player.show_dialogue("Уровень спор - %s" % Global.current_spore_level)
+		#else:
+			#player.show_dialogue("Прибор молчит")
+			#
+		## Сигнал интерфейсу на снятие заряда и запуск перезарядки
+		#Events.item_used.emit(current_used_item_id)
+	#else:
+		#print("denied_get_current_level")
+		#player.show_dialogue("Прибор молчит")
+		#
+		## Сигнал интерфейсу на снятие заряда и запуск перезарядки
+		#Events.item_used.emit(current_used_item_id)
+
+
+func physics_process(_delta: float) -> void:
+	# В этом методе мы просто ждем, пока закончится анимация.
+	# Самый простой способ (без сигналов) — проверить, проигрывается ли она еще:
+	if not player.anim_player.is_playing() or player.anim_player.current_animation != "use_item":
+		# Как только анимация кончилась, возвращаемся в покой
+		switch_state.emit(idle_state)
+
+
+func get_spore_level(current_used_item_id: int) -> void:
 	if player.at_spore_area:
-		print("get_current_spore_level")
+		print("use_item_state.get_spore_level.get_current_spore_level: ", Global.current_spore_level)
 		if Global.current_spore_level:
 			player.show_dialogue("Уровень спор - %s" % Global.current_spore_level)
 		else:
@@ -48,11 +83,3 @@ func enter_state() -> void:
 		
 		# Сигнал интерфейсу на снятие заряда и запуск перезарядки
 		Events.item_used.emit(current_used_item_id)
-
-
-func physics_process(_delta: float) -> void:
-	# В этом методе мы просто ждем, пока закончится анимация.
-	# Самый простой способ (без сигналов) — проверить, проигрывается ли она еще:
-	if not player.anim_player.is_playing() or player.anim_player.current_animation != "use_item":
-		# Как только анимация кончилась, возвращаемся в покой
-		switch_state.emit(idle_state)
