dd5dc8c93ceeab7501d5f2edaf7ae526b485dfe4
NNE9376 <vensovn@gmail.com>
2026-07-28 09:16:51 +0300

    chore: Удалить вывод сообщения

--- src/core/autoload/dwellers.gd
+++ src/core/autoload/dwellers.gd
@@ -75,7 +75,8 @@ var misleading_lines = [
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Dwellers: ", hints_dialog_lines[0] % Global.infected_apartment)
+	# print("Dwellers: ", hints_dialog_lines[0] % Global.infected_apartment)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
