bcfa10921b96d23b3cab75f2d5249f49d0897c40
NNE9376 <vensovn@gmail.com>
2026-08-06 18:56:02 +0300

    chore: Добавить обновленные значения таймеров

--- src/core/autoload/global.gd
+++ src/core/autoload/global.gd
@@ -4,15 +4,15 @@ extends Node
 var transition_duration: float = 3.0
 
 const TOTAL_FLOORS: int = 3
-const LIFT_TIMER_WAIT_TIME: float = 30.0
+const LIFT_TIMER_WAIT_TIME: float = 60.0
 const LIFT_TIMER_ONE_SHOT: bool = true
 
 var has_anomaly: bool
 var anomaly_located_floor: int
-const ANOMALY_LIFT_ACTIVE_LOW: float = 10
+const ANOMALY_LIFT_ACTIVE_LOW: float = 15
 const ANOMALY_LIFT_ACTIVE_HIGH: float = 30
 
-const TIMER_ACTIVATE_SPORE: float = 25.0
+const TIMER_ACTIVATE_SPORE: float = 20.0
 const TIMER_ACTIVATE_SPORE_NEW: float = 10.0
 
 var infected_apartment: int
